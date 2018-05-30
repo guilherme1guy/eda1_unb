94145144de89b6efae6fd701a2151281c9cff114
guilherme1guy <guilhermeguy349@gmail.com>
2018-05-30 16:43:14 -0300

    Add project 4 structure

---
+++ project4/src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+
+#include "plane.h"
+
+
+int main() {
+	
+	puts("Hello world!\n");
+
+
+	return 0;
+}
